0d523d3903af9560c760f91f0c1ae47180560508
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-01 02:05:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+def sort_array_asc(array)
+array.sort do |a,b|
+   a<=>b
+end
+end 
 
+def sort_array_desc(array)
+
+array.sort do |a,b|
+   b<=>a
+end
+end 
+
+def sort_array_char_count(array)
+array.sort do |a,b|
+   a<=>b
+end
+end
+
+def swap_elements(array)
+  temp=array[1]
+  array[1]=array[2]
+  array[2]=temp
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def kesha_maker(array)
+s=""
+a=Array.new
+array.each do |element|
+s=element
+s.insert(2,'$')
+a.push(s)
+end
+end 
